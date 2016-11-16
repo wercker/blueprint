02235177cb57eee5790013cd34cf6dfed6488244
termie <github@anarkystic.com>
2016-11-15 18:42:19 -0800

    update grpc, go, and put shell in a .sh

---
+++ templates/service/core/generate-protobuf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+echo "Generating gRPC server"
+protoc -I/usr/local/include \
+  -I. \
+  -I$GOPATH/src \
+  -I../vendor \
+  -I../vendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --go_out=Mgoogle/api/annotations.proto=github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:. \
+  blueprint.proto
+
+echo "Generating gateway"
+protoc -I/usr/local/include \
+  -I. \
+  -I$GOPATH/src \
+  -I../vendor \
+  -I../vendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --grpc-gateway_out=logtostderr=true:. \
+  blueprint.proto
+
+echo "Generating swagger"
+protoc -I/usr/local/include \
+  -I. \
+  -I$GOPATH/src \
+  -I../vendor \
+  -I../vendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --swagger_out=logtostderr=true:. \
+  blueprint.proto
