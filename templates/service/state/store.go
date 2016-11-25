c007092ba28a78317d3755dfc1be6e02389dfd90
Benno van den Berg <bennovandenberg@gmail.com>
2016-11-25 14:10:40 +0100

    Replace auth with blueprint

--- templates/service/state/store.go
+++ templates/service/state/store.go
@@ -2,7 +2,7 @@ package state
 
 import "io"
 
-// Store provides access to data that is required for auth.
+// Store provides access to data that is required for blueprint.
 type Store interface {
 	io.Closer
 }
