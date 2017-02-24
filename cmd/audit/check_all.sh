c404e2b852eaebeb55f83cf0cdbea20346e981e8
termie <github@anarkystic.com>
2017-02-24 10:56:18 +0100

    helper script, commented some stuff out

--- cmd/audit/check_all.sh
+++ cmd/audit/check_all.sh
@@ -5,7 +5,13 @@ LIST="ark auth cluster-manager enable-feature envvars inspector inviter kiddie-p
 for x in $LIST; do
   echo "./audit $x"
   echo
-
+  #(
+  #cd "$GOPATH/src/github.com/wercker/$x" || exit
+  #git status
+  #sleep 5
+  #git checkout master
+  #git pull origin master
+  #)
   ./audit $GOPATH/src/github.com/wercker/$x
 
   echo
