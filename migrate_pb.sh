9499cac4236a0bb216012e1116cb1130b2f538e1
termie <github@anarkystic.com>
2017-09-19 13:18:09 -0700

    add docs

--- migrate_pb.sh
+++ migrate_pb.sh
@@ -1,5 +1,53 @@
 #!/bin/bash
 
+# This is a tool to migrate protocol buffers for a
+# blueprint-like project from being in the `core/blueprint.proto`
+# to being in `blueprint.proto` and putting the output into
+# `blueprintpb/blueprint.pb.go`.
+
+# Example usage output:
+#termie@golfer:github.com/wercker/trigger % ../blueprint/migrate_pb.sh trigger trigger state queue providers events consumer clustermanager [pbswitch] 11:02:22
+#Transforming trigger to trigger...
+#$fake git mv core/$OLD.proto ./$NEW.proto
+#$fake git rm -f core/$OLD.*
+#rm 'core/trigger.pb.go'
+#rm 'core/trigger.pb.gw.go'
+#rm 'core/trigger.swagger.json'
+#$fake git mv core ${NEW}pb
+#$fake sed -i s/core/${NEW}pb/g ${NEW}pb/proto.go
+#$fake sed -i s/$OLD\\\\/${NEW}\\\\/g wercker.yml
+#$fake sed "s/blue_print/$NEW/g" $ROOT/templates/service/blue_printpb/generate-protobuf.sh > ${NEW}pb/generate-protobuf.sh
+#
+#
+#while (( "$#" )); do
+#  replace_dir $1
+#  shift
+#done
+#Replacing core -> triggerpb in state/store.go
+#Replacing core -> triggerpb in consumer/consumer.go
+#
+#set +v
+#termie@golfer:github.com/wercker/trigger % git status                                                                                      [pbswitch] 11:02:26
+#On branch pbswitch
+#Changes to be committed:
+#  (use "git reset HEAD <file>..." to unstage)
+#
+#	deleted:    core/trigger.pb.go
+#	deleted:    core/trigger.pb.gw.go
+#	deleted:    core/trigger.swagger.json
+#	renamed:    core/trigger.proto -> trigger.proto
+#	renamed:    core/generate-protobuf.sh -> triggerpb/generate-protobuf.sh
+#	renamed:    core/proto.go -> triggerpb/proto.go
+#
+#Changes not staged for commit:
+#  (use "git add <file>..." to update what will be committed)
+#  (use "git checkout -- <file>..." to discard changes in working directory)
+#
+#	modified:   consumer/consumer.go
+#	modified:   state/store.go
+#	modified:   triggerpb/generate-protobuf.sh
+#	modified:   triggerpb/proto.go
+
 set -e
 
 ROOT=$(dirname $BASH_SOURCE)
@@ -8,7 +56,9 @@ OLD=$1
 NEW=$2
 
 if [[ -z "$OLD" || -z "$NEW" ]]; then
-  echo "Need at least two args: new_pb.sh <old> <new> [<dir> <dir>]"
+  echo "Need at least two args: migrate_pb.sh <old> <new> [<dir to find-replace> <dir to find-replace>]"
+  echo "Example usage:"
+  echo "  ../blueprint/migrate_pb.sh trigger trigger state"
   exit 1
 fi
 
--- sync_vendor.sh
+++ sync_vendor.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# This script syncs the versions of libraries in the current vendor to the
+# "official" versions in the blueprint template source.
+
+# Example usage:
+#  cd $GOPATH/src/github.com/wercker/trigger
+#  ../blueprint/sync_vendor.sh
+
 ROOT=$(dirname $BASH_SOURCE)
 jq '.package[] | "\(.path)@\(.revision)"' < $ROOT/templates/service/vendor/vendor.json | xargs -n 1 govendor fetch
